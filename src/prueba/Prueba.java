fd67075bf6eb40903c2d1acee01b23f7f4f001ac
gitmarroyo <miguel.marroyo@yahoo.es>
2017-07-24 23:36:50 +0200

    as

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -11,6 +11,6 @@ public class Prueba extends JApplet{
 	public void paint(Graphics g) {
 	        g.drawString("Hola, mundo!", 45, 95);
 	        g.drawString("Aquí estamos practicando!", 45, 105);
-	        g.drawString("Aquí estamos intentando hacer un repositorio!", 45, 125);
+	        g.drawString("Aquí estamos intentando hacer repositorio!", 45, 125);
 	    }
 }
