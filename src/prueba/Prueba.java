7623c3a45e1494ad70a147482d86c5bb800e9334
gitmarroyo <miguel.marroyo@yahoo.es>
2017-07-24 19:30:46 +0200

    Primera versión

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,14 @@
+package prueba;
+
+import java.awt.Graphics;
+import javax.swing.JApplet;
+
+public class Prueba extends JApplet{
+
+	private static final long serialVersionUID = 1L;
+
+	public void paint(Graphics g) {
+	        g.drawString("Hola, mundo!", 45, 95);
+	        g.drawString("Aquí estamos practicando!", 45, 105);
+	    }
+}
