02534fcc828e4b7121e12e4c4c763d4b03cbc046
gitmarroyo <miguel.marroyo@yahoo.es>
2017-07-25 12:05:29 +0200

    Prueba Rama-1
    Change-Id: I992fa7a49a6dbeaf7e121b95951dc84be4fac2a8
    Change-Id: I001

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -13,6 +13,6 @@ public class Prueba extends JApplet{
 	        g.drawString("Aquí estamos practicando!", 45, 105);
 	        g.drawString("Aquí estamos intentando hacer repositorio!", 45, 125);
 	        g.drawString("Prueba repositorio 1", 45, 145);
-	        g.drawString("Prueba Rama-2", 45, 145);
+	        g.drawString("Prueba Rama-1", 45, 160);
 	    }
 }
