f60efda3f04a47d04ff185e5b77724418542a447
gitmarroyo <miguel.marroyo@yahoo.es>
2017-08-08 12:42:56 +0200

    Se añade operación aritmética
    Change-Id: I5f958d8afdf45ca59a1810922478bd0f4c4b2001
    Change-Id: I003

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -1,6 +1,7 @@
 package prueba;
 
 import java.awt.Graphics;
+
 import javax.swing.JApplet;
 
 public class Prueba extends JApplet{
@@ -9,10 +10,19 @@ public class Prueba extends JApplet{
 
 	@Override
 	public void paint(Graphics g) {
+			float x = 1;
+			char r;
 	        g.drawString("Hola, mundo!", 45, 95);
 	        g.drawString("Aquí estamos practicando!", 45, 105);
 	        g.drawString("Aquí estamos intentando hacer repositorio!", 45, 125);
-	        g.drawString("Prueba repositorio 1", 45, 145);
-	        g.drawString("Prueba Rama-2", 45, 145);
+	        g.drawString("Prueba repositorio 1", 45, 150);
+	        g.drawString("Prueba Rama-2", 45, 170);
+	        x = x+1;
+	        ++x;
+	        x=x/2;
+	        r= (char) x;
+	        g.drawString("El estimado", 45, 190);
+	        System.out.println("Resultado de la operación: " + x);
+	        System.out.println("Resultado de la operación 2: " + r);
 	    }
 }
