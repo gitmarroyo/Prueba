d93d060fd4f444886e4f2c3b84ef09a19e32b367
gitmarroyo <miguel.marroyo@yahoo.es>
2017-08-09 11:49:13 +0200

    Change-Id: Iccceeb4e58d74d7bf1894ab8e24d53fcf2a3308d

--- src/Operaciones/Operaciones.java
+++ src/Operaciones/Operaciones.java
@@ -9,15 +9,15 @@ public class Operaciones {
 
 		x = x + 1;
 		++x;
-		System.out.println("Resultado de la operación ++x : " + x);
+		System.out.println("Resultado de la operación ++x   : " + x);
 		x = x / 2;
 		System.out.println("Resultado de la operación x=x/2 : " + x);
 		x += 1;
-		System.out.println("Resultado de la operación x+=1 : " + x);
+		System.out.println("Resultado de la operación x+=1  : " + x);
 		x -= 2;
-		System.out.println("Resultado de la operación x-=2 : " + x);
+		System.out.println("Resultado de la operación x-=2  : " + x);
 		x++;
-		System.out.println("Resultado de la operación x++ : " + x);
+		System.out.println("Resultado de la operación x++   : " + x);
 		System.out.println();
 	}
 }
